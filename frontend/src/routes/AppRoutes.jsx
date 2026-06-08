5072007633b0aaf67aa62015be850200fd20337d
Doumbouya94 <sidiki940917@gmail.com>
2026-06-08 14:26:57 -0400

    Add Admin Dashboard component and route

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -7,20 +7,23 @@ import Products from "../features/products-page/Products";
 import Cart from "../features/cart/Cart";
 import Gallery from "../features/gallery-portfolio/Gallery";
 import Contact from "../features/contact-us/Contact";
+import Admin from "../features/admin/Admin";
 
 const AppRoutes = () => {
   return (
     <>
       <Navbar />
       <main style={{ paddingTop: "64px" }}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* Route pour le dashboard admin */}
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
       </main>
     </>
   );
